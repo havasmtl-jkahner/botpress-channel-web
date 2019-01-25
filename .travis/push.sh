526b2d01d397b33d9b8b8e3935fb4ee6ed58ff9e
Jesse Kahner <36279590+havasmtl-jkahner@users.noreply.github.com>
2019-01-25 16:10:36 -0500

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -14,7 +14,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git > /dev/null 2>&1
+  git remote add origin "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git" > /dev/null 2>&1
   git push --quiet --set-upstream origin build-dev
   #git push "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git" master > /dev/null 2>&1
 }

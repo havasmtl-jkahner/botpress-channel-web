59423e16e868d4c472f6e5005b591ddc7f72ffe8
Jesse Kahner <36279590+havasmtl-jkahner@users.noreply.github.com>
2019-01-28 16:46:53 -0500

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -7,19 +7,20 @@ setup_git() {
 
 commit_website_files() {
  #git remote
+ git pull
  git checkout -b master
+ git pull
 # git checkout master
  #git config credential.helper store
  #git config --global user.email "jesse.kahner@havas.com"
  #git config --global user.name "havasmtl-jkahner"
-  git add . *.js
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add . *.js && git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git status
 }
 
 upload_files() {
-git remote -v
-git remote remove origin-build
+  git remote -v
+  git remote remove origin-build
 # git remote add origin git@github.com:havasmtl-jkahner/botpress-channel-web.git
 # git remote -v
   git remote add origin-build "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git"

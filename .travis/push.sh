abc837c72dffdab84da1a508067e898e687871d5
Jesse Kahner <36279590+havasmtl-jkahner@users.noreply.github.com>
2019-01-29 09:35:31 -0500

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,21 +6,26 @@ setup_git() {
 }
 
 commit_website_files() {
-  git remote remove origin-build
-  git remote add origin-build "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git"
-  git remote -v
-  git fetch
-  git checkout -b master
-  git pull origin master
+  git checkout -b gh-pages
   git add . *.js
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git status
-  git push --quiet --set-upstream origin-build master
+#   git remote remove origin-build
+#   git remote add origin-build "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git"
+#   git remote -v
+#   git fetch
+#   git checkout -b master
+#   git pull origin master
+#   git add . *.js
+#   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#   git status
+#   git push --quiet --set-upstream origin-build master
 }
 
 upload_files() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+#   git config --global user.email "travis@travis-ci.org"
+#   git config --global user.name "Travis CI"
+  git remote add origin-pages "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git" > /dev/null 2>&1
+  git push --quiet --set-upstream origin-pages gh-pages
 }
 
 setup_git

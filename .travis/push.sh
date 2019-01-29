8ad5e02831932e02d6e5d8e86331991e058891f2
Jesse Kahner <36279590+havasmtl-jkahner@users.noreply.github.com>
2019-01-29 09:03:59 -0500

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
+  
 }
 
 commit_website_files() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
  #git remote
   git remote -v
   git remote remove origin-build
@@ -14,19 +15,21 @@ commit_website_files() {
 git remote -v
  git fetch
  git checkout -b master
- git pull
+ git pull origin master
 # git checkout master
  #git config credential.helper store
  #git config --global user.email "jesse.kahner@havas.com"
  #git config --global user.name "havasmtl-jkahner"
   git add . *.js && git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
   git status
+  git push --quiet --set-upstream origin-build master
+  
 }
 
 upload_files() {
  
   #git push --set-upstream origin master
-  git push --quiet --set-upstream origin-build master
+#   git push --quiet --set-upstream origin-build master
   #git push "https://${GH_TOKEN}@github.com/havasmtl-jkahner/botpress-channel-web.git" master > /dev/null 2>&1
 #   git push
 }
